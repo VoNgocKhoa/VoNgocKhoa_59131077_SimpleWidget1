dadd0c704ff51965463f55edfae8e1f69fea84bb
VoNgocKhoa <63081058+VoNgocKhoa@users.noreply.github.com>
2020-04-24 18:02:10 +0700

    Initial commit

---
+++ app/src/main/java/vn/edu/ntu/vongockhoa/vongockhoa_59131077_simplewidget/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.ntu.vongockhoa.vongockhoa_59131077_simplewidget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+    }
+}
